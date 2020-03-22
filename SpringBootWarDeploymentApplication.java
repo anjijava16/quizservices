32f72331e8067dce5b36210ffd5aff159ad62215
Anjaiah Methuku <anjijava16@users.noreply.github.com>
2020-03-21 21:05:40 -0400

    Create SpringBootWarDeploymentApplication.java

---
+++ SpringBootWarDeploymentApplication.java
@@ -0,0 +1,20 @@
+package com.mmtech.quiz;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
+	
+@SpringBootApplication
+public class SpringBootWarDeploymentApplication  extends SpringBootServletInitializer {
+	
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWarDeploymentApplication.class);
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWarDeploymentApplication.class, args);
+	}
+}
